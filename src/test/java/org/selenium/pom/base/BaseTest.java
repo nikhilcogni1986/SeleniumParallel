6f9a579dc4b6c807999a6da3eacb990bbddd8596
NIKHIL <nikhilcogni@gmail.com>
2022-08-24 15:58:48 +0100

    Added screenshots methods

--- src/test/java/org/selenium/pom/base/BaseTest.java
+++ src/test/java/org/selenium/pom/base/BaseTest.java
@@ -1,41 +1,57 @@
 package org.selenium.pom.base;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.selenium.pom.factory.DriverManager;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
-public class BaseTest
-{
-    private ThreadLocal <WebDriver> driver = new ThreadLocal<>();
+import java.io.File;
+import java.io.IOException;
 
-    private void setDriver(WebDriver driver)
-    {
+public class BaseTest {
+    private ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    private void setDriver(WebDriver driver) {
         this.driver.set(driver);
     }
 
-    protected WebDriver getDriver()
-    {
+    protected WebDriver getDriver() {
         return this.driver.get();
     }
 
     @Parameters("browser")
     @BeforeMethod
-    public void startDriver(@Optional String browser)
-    {
-        if(browser==null)
+    public void startDriver(@Optional String browser) {
+        if (browser == null)
             browser = "CHROME";
-        System.getProperty("browser",browser);
+        System.getProperty("browser", browser);
         setDriver(new DriverManager().initializeDriver(browser));
-        System.out.println("CURRENT THREAD: "+Thread.currentThread().getId()+ ", "+"DRIVER ="+getDriver());
+        System.out.println("CURRENT THREAD: " + Thread.currentThread().getId() + ", " + "DRIVER =" + getDriver());
     }
 
+    @Parameters("browser")
     @AfterMethod
-    public void quitDriver()
-    {
-        System.out.println("CURRENT THREAD: "+Thread.currentThread().getId()+ ", "+"DRIVER ="+getDriver());
+    public void quitDriver(ITestResult result, @Optional String browser) throws IOException {
+        System.out.println("CURRENT THREAD: " + Thread.currentThread().getId() + ", " + "DRIVER =" + getDriver());
+        if (result.getStatus() == ITestResult.FAILURE) {
+            File destFile = new File("scr" + File.separator + browser + File.separator +
+                    result.getTestClass().getRealClass().getSimpleName() + File.separator
+            + result.getMethod().getMethodName() + ".png");
+            takeScreenshot(destFile);
+        }
         getDriver().quit();
+    }
+
+    private void takeScreenshot(File destFile) throws IOException {
+        TakesScreenshot takesScreenshot = (TakesScreenshot) getDriver();
+        File srcFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(srcFile, destFile);
     }
 }
